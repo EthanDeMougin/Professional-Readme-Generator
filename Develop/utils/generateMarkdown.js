0cb1a2558466c45615f0009e52e4c12ab60860c7
Ethan DeMougin <ethandemougin@gmail.com>
2022-11-03 09:53:45 -0400

    table of contents and content for readme

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,8 +1,6 @@
 function generateMarkdown(data) {
-  //license
   let licenseOption = `${data.license}`;
   let licenseLink = '';
-  //if conditional to give the license the correct link
   if (licenseOption === 'GNU AGPLv3') {
     licenseOption = 'GNUAGPLv3';
     licenseLink = 'https://choosealicense.com/licenses/agpl-3.0/';
@@ -36,8 +34,117 @@ function generateMarkdown(data) {
     licenseLink = 'https://choosealicense.com/licenses/unlicense/';
   };
 
-//create variable to hold markdown template and dynamically add to it
-//let markdownTemplate =
+let markdownTemplate =
 
+`# ${data.title}
+## Description
+${data.description}
+![badge](https://img.shields.io/badge/license-${licenseOption}-brightorange)
+You can access more badges and their purposes at [shields.io](https://shields.io)
+`;
+
+let tableOfContents =
+`## Table of Contents`;
+  if (data.installation) {
+    tableOfContents +=
+      `
+  * [Installation](#installation)`
+  };
+  if (data.instructions) {
+    tableOfContents +=
+      `
+  * [Usage](#usage)`
+  };
+  if (data.contribution) {
+    tableOfContents +=
+      `
+  * [Contribution](#contribution)`
+  };
+  if (data.testing) {
+    tableOfContents +=
+      `
+  * [Testing](#testing)`
+  };
+
+  //append table to template
+  markdownTemplate += tableOfContents;
+
+  //add contact info and license (required)
+  markdownTemplate +=
+    `
+  * [Questions](#questions)`;
+  markdownTemplate +=
+    `
+  * [License](#license)
+    
+    `;
+
+  //add optional sections content
+
+  //installation
+  if (data.installation) {
+    markdownTemplate +=
+      `
+## Installation
+    
+  _Follow these steps to properly install this application:_
+  ${data.installation}`
+  };
+
+  //usage
+  if (data.instructions) {
+    markdownTemplate +=
+      `
+      
+## Usage
+  _Instructions for use:_
+  ${data.instructions}`
+  };
+
+  //contributions
+  if (data.contribution) {
+    markdownTemplate +=
+      `
+      
+## Contribution
+  _If you would like to contribute, please adhere to these guidelines:_
+  ${data.contribution}`
+  };
+
+  //testing
+  if (data.testing) {
+    markdownTemplate +=
+      `
+      
+## Testing
+  _Instructions for testing application:_
+  ${data.testing}`
+  };
+
+  //questions
+    markdownTemplate +=
+      `
+      
+## Questions
+      
+  _For further questions:_
+  ${data.questions}
+  
+  _Contact Info:_
+  GitHub: [${data.username}](https://github.com/${data.username})
+  Email: [${data.email}](mailto:${data.email})`;
+  
+  markdownTemplate +=
+    `
+    
+## License
+      
+  _This application has the ${data.license}._
+      
+  For more information please view the [license description](${licenseLink}).
+  
+  `;
+  return markdownTemplate;
+}
 
 module.exports = generateMarkdown;
